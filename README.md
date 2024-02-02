7b34c927c501d3e513d27c3f963ce92a8e38e149
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2024-02-02 09:46:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CodeAlpha_FlashCard_Quiz_App
+ This a flutter application for studying. Users can add flashcards with questions and answers then quiz themselves on the material. Track and display quiz scores
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flash_card_quiz_app/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
