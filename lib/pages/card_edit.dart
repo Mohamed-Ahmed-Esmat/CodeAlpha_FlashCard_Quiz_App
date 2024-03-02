8356de9c59212e97fd5751d3f56c50471b7ebeaf
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2024-03-02 11:59:58 +0200

    edit some input from excel

--- lib/pages/card_edit.dart
+++ lib/pages/card_edit.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:csv/csv.dart';
 import 'package:excel/excel.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -16,12 +15,12 @@ class CardEditPage extends StatefulWidget {
   final List<String>? answers;
 
   const CardEditPage({
-    super.key,
+    Key? key,
     this.title,
     this.questions,
     this.answers,
     this.cardId,
-  });
+  }) : super(key: key);
 
   @override
   _CardEditPageState createState() => _CardEditPageState();
@@ -32,6 +31,7 @@ class _CardEditPageState extends State<CardEditPage> {
   TextEditingController titleController = TextEditingController();
   List<TextEditingController> questionControllers = [];
   List<TextEditingController> answerControllers = [];
+  bool isLoading = false; // New variable to track loading state
   final FirestoreService _firestoreService = FirestoreService();
 
   @override
@@ -58,26 +58,21 @@ class _CardEditPageState extends State<CardEditPage> {
     List<String> answers =
         answerControllers.map((controller) => controller.text).toList();
 
-    // Check if the title, questions, and answers are not empty
     if (titleController.text.isNotEmpty &&
         questions.isNotEmpty &&
         answers.isNotEmpty &&
         questions[0].isNotEmpty &&
         answers[0].isNotEmpty) {
       if (widget.cardId == null) {
-        // Save the new card to Firestore
         await _firestoreService.addFlashyCard(
             titleController.text, questions, answers);
       } else {
-        // Edit the existing card in Firestore
         await _firestoreService.editFlashyCard(
             widget.cardId!, titleController.text, questions, answers);
       }
 
-      // After saving or editing the card, navigate back to the CardListPage
       Navigator.pop(context, 'refresh');
     } else {
-      // Show a dialog if the title, questions, or answers are empty
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -98,23 +93,24 @@ class _CardEditPageState extends State<CardEditPage> {
   }
 
   Future<void> pickAndUploadFile() async {
+    setState(() {
+      isLoading = true; // Set loading state to true when picking the file
+    });
+
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
-      allowedExtensions: ['csv', 'xlsx'],
+      allowedExtensions: ['xlsx'],
     );
 
     if (result != null) {
       String filePath = result.files.single.path!;
-      // Process the file using filePath
       Map<String, List<String>> fileData = await _readFile(filePath);
 
       if (fileData['questions']!.isNotEmpty &&
           fileData['answers']!.isNotEmpty) {
-        // Clear the existing controllers
         questionControllers.clear();
         answerControllers.clear();
 
-        // Update the controllers with the new data
         for (var i = 0; i < fileData['questions']!.length; i++) {
           questionControllers
               .add(TextEditingController(text: fileData['questions']![i]));
@@ -122,9 +118,14 @@ class _CardEditPageState extends State<CardEditPage> {
               .add(TextEditingController(text: fileData['answers']![i]));
         }
 
-        // Update the UI
-        setState(() {});
+        setState(() {
+          isLoading = false; // Set loading state to false after data is loaded
+        });
       }
+    } else {
+      setState(() {
+        isLoading = false; // Set loading state to false if no file is picked
+      });
     }
   }
 
@@ -132,34 +133,9 @@ class _CardEditPageState extends State<CardEditPage> {
     List<String> questions = [];
     List<String> answers = [];
 
-    if (filePath.endsWith('.csv')) {
-      // Read CSV file
-      String fileContent = await File(filePath).readAsString();
-      List<List<dynamic>> csvData = CsvToListConverter().convert(fileContent);
-
-      var cellCounter = 0;
-      var rowCounter = 0;
-      for (var row in csvData) {
-        if (rowCounter == 0) {
-          rowCounter++;
-          continue;
-        }
-        for (var cell in row) {
-          final cellValue = cell.toString();
-          if (cellCounter % 2 == 0) {
-            questions.add(cellValue);
-            cellCounter++;
-          } else {
-            answers.add(cellValue);
-            cellCounter = 0;
-          }
-        }
-      }
-    } else if (filePath.endsWith('.xlsx')) {
-      // Read XLSX file
+    if (filePath.endsWith('.xlsx')) {
       var bytes = File(filePath).readAsBytesSync();
       var excel = Excel.decodeBytes(bytes);
-      var cellCounter = 0;
       var rowCounter = 0;
       for (var table in excel.tables.keys) {
         for (var row in excel.tables[table]!.rows) {
@@ -167,16 +143,17 @@ class _CardEditPageState extends State<CardEditPage> {
             rowCounter++;
             continue;
           }
-          for (var cell in row) {
-            final cellValue = cell?.value;
-            if (cellCounter % 2 == 0) {
-              questions.add(cellValue.toString());
-              cellCounter++;
-            } else {
-              answers.add(cellValue.toString());
-              cellCounter = 0;
+          if (row.length >= 2) {
+            // Read only the first two cells of each row
+            var questionValue = row[0]?.value;
+            var answerValue = row[1]?.value;
+
+            if (questionValue != null && answerValue != null) {
+              questions.add(questionValue.toString());
+              answers.add(answerValue.toString());
             }
           }
+          rowCounter++;
         }
       }
     }
@@ -196,83 +173,88 @@ class _CardEditPageState extends State<CardEditPage> {
           ),
         ],
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: titleController,
-              decoration: InputDecoration(
-                labelText: 'Title',
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          const Divider(
-            thickness: 2.0, // Increased thickness
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: questionControllers.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextField(
-                      controller: questionControllers[index],
-                      decoration: InputDecoration(
-                        labelText: 'Question ${index + 1}',
-                        border: OutlineInputBorder(),
-                      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(), // Show circular indicator
+            )
+          : ListView(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                  SizedBox(height: 12.0), // Added SizedBox here
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
-                    child: TextField(
-                      controller: answerControllers[index],
-                      decoration: InputDecoration(
-                        labelText: 'Answer ${index + 1}',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    ),
+                ),
+                const Divider(
+                  thickness: 2.0,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: questionControllers.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: TextField(
+                            controller: questionControllers[index],
+                            decoration: InputDecoration(
+                              labelText: 'Question ${index + 1}',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 12.0),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                          child: TextField(
+                            controller: answerControllers[index],
+                            decoration: InputDecoration(
+                              labelText: 'Answer ${index + 1}',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              questionControllers.removeAt(index);
+                              answerControllers.removeAt(index);
+                            });
+                          },
+                        ),
+                        const Divider(
+                          thickness: 1.0,
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FloatingActionButton(
                     onPressed: () {
                       setState(() {
-                        questionControllers.removeAt(index);
-                        answerControllers.removeAt(index);
+                        questionControllers.add(TextEditingController());
+                        answerControllers.add(TextEditingController());
                       });
                     },
+                    child: Icon(Icons.add),
+                    backgroundColor: Colors.blue,
                   ),
-                  const Divider(
-                    thickness: 1.0, // You can adjust thickness here
-                  ),
-                ],
-              );
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  questionControllers.add(TextEditingController());
-                  answerControllers.add(TextEditingController());
-                });
-              },
-              child: Icon(Icons.add),
-              backgroundColor: Colors.blue,
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: saveCard,
         child: Icon(Icons.save),
